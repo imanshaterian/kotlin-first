4954c164397996f7e8edda3afb7196554414256e
iman <imanshaterian@gmail.com>
2022-11-27 11:12:04 +0100

    changed the main file to show happy birthday massage

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.birthdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.birthdaycard.ui.theme.BirthdayCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    HappyBirthDayCard("sam","iman")
                 }
             }
         }
@@ -30,14 +32,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HappyBirthDayCard(name: String, from: String) {
+    Column {
+        Text("Happy birthday $name!", fontSize = 36.sp)
+        Text("From $from!", fontSize = 24.sp)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BirthdayCardTheme {
-        Greeting("Android")
+        HappyBirthDayCard( "sam","iman")
+
     }
 }
