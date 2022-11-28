cf0485a34c17c243b46023943e9319a8f6174a38
iman <imanshaterian@gmail.com>
2022-11-28 11:35:58 +0100

    adding the birthday picture and setting the text to shown above it.

--- app/src/main/java/com/example/birthdaycard/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.birthdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.birthdaycard.ui.theme.BirthdayCardTheme
 
@@ -24,18 +28,38 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    HappyBirthDayCard("sam","iman")
+                    HappyBirthdayPicture("sam","iman")
                 }
             }
         }
     }
 }
-
+@Composable
+fun HappyBirthdayPicture(name: String, from: String){
+    Box {
+        Image(painter = painterResource(id = R.drawable.androidparty),
+            contentDescription = null,
+        modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth(),
+        contentScale = ContentScale.Crop)
+        HappyBirthDayCard(name = name, from = from )
+    }
+}
 @Composable
 fun HappyBirthDayCard(name: String, from: String) {
     Column {
-        Text("Happy birthday $name!", fontSize = 36.sp)
-        Text("From $from!", fontSize = 24.sp)
+        Text("Happy birthday $name!",
+            fontSize = 36.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(align = Alignment.CenterHorizontally)
+                .padding(8.dp)
+        )
+        Text("From $from!",
+            fontSize = 24.sp
+            , modifier = Modifier
+                .fillMaxWidth())
     }
 }
 
@@ -43,7 +67,7 @@ fun HappyBirthDayCard(name: String, from: String) {
 @Composable
 fun DefaultPreview() {
     BirthdayCardTheme {
-        HappyBirthDayCard( "sam","iman")
+        HappyBirthdayPicture( "sam","iman")
 
     }
 }
